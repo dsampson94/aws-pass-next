e4a13323d342f58a9bda1691919c6eea5aa2375c
dsampson-cs <david.sampson@cloudsmiths.global>
2024-09-21 23:28:22 +0200

    adds icons

--- components/Header.tsx
+++ components/Header.tsx
@@ -3,6 +3,7 @@
 import { FaMoon, FaSun } from 'react-icons/fa';
 import { useTheme } from 'next-themes';
 import { useEffect, useState } from 'react';
+import Image from 'next/image';
 
 export default function Header() {
     const { theme, setTheme } = useTheme();
@@ -18,8 +19,15 @@ export default function Header() {
         <header className={ `${ theme === 'light' ? 'bg-white' : 'bg-gray-900' } text-white shadow-md` }>
             <nav className="container px-4 flex justify-between items-center py-4 min-w-full">
                 <a href="/"
-                   className={ `text-xl font-bold ${ theme === 'light' ? 'text-black' : 'text-white' }` }>
-                    AWS Pass
+                   className={ `flex items-center space-x-2 text-xl font-bold ${ theme === 'light' ? 'text-black' : 'text-white' }` }>
+                    <Image
+                        src='/awspass.png'
+                        alt='AWS Pass Logo'
+                        width={ 40 }
+                        height={ 40 }
+                        className="mb-0"
+                    />
+                    <span>AWS Pass</span>
                 </a>
 
                 <div className="flex space-x-4 items-center">
@@ -31,19 +39,19 @@ export default function Header() {
                         { theme === 'light' ? <FaMoon/> : <FaSun/> }
                     </button>
 
-                    {/*<a*/}
-                    {/*    href="https://buymeacoffee.com/awsquiztool"*/}
-                    {/*    target="_blank"*/}
-                    {/*    rel="noopener noreferrer"*/}
-                    {/*    aria-label="Buy me a coffee"*/}
-                    {/*    className="flex items-center"*/}
-                    {/*>*/}
-                    {/*    <img*/}
-                    {/*        src="https://cdn.buymeacoffee.com/buttons/v2/default-yellow.png"*/}
-                    {/*        alt="Buy Me a Coffee"*/}
-                    {/*        className="h-8 w-auto rounded-lg hover:opacity-90 transition-opacity duration-300"*/}
-                    {/*    />*/}
-                    {/*</a>*/}
+                    {/*<a*/ }
+                    {/*    href="https://buymeacoffee.com/awsquiztool"*/ }
+                    {/*    target="_blank"*/ }
+                    {/*    rel="noopener noreferrer"*/ }
+                    {/*    aria-label="Buy me a coffee"*/ }
+                    {/*    className="flex items-center"*/ }
+                    {/*>*/ }
+                    {/*    <img*/ }
+                    {/*        src="https://cdn.buymeacoffee.com/buttons/v2/default-yellow.png"*/ }
+                    {/*        alt="Buy Me a Coffee"*/ }
+                    {/*        className="h-8 w-auto rounded-lg hover:opacity-90 transition-opacity duration-300"*/ }
+                    {/*    />*/ }
+                    {/*</a>*/ }
                 </div>
             </nav>
         </header>
