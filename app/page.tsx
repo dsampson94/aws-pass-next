6ac98a009cad7b19a56d451b51ca4f6019a06c50
dsampson-cs <david.sampson@cloudsmiths.global>
2024-09-23 15:08:15 +0200

    adds vercel analytics

--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,13 @@
 'use client';
 
 import Hero from '../components/Hero';
+import { Analytics } from '@vercel/analytics/react';
 
 export default function Home() {
     return (
         <div>
             <Hero/>
+            <Analytics/>
         </div>
     );
 }
